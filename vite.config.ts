2db5d76ed0e2643df96e48cc66b04ce8e4b92ba1
Programador <vtex@subeagenciadigital.com>
2025-02-25 12:45:23 +0100

    feat:pages github

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ccnorte/", // 👈 Add this line (replace with your actual repo name)
+  build: {
+    outDir: "dist"
+  }
 })
